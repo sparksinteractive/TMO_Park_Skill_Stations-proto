ea1b1ac19d7cfbf2226aacccabd712f4f1bfed9a
5484f90d2e8e272e31edc5044ff2b20b660d21d9 <marcusguttenplan@gmail.com>
2019-11-12 14:46:05 -0500

    add realsense code

--- index.js
+++ index.js
@@ -4,34 +4,215 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const http = require('http');
 const cors = require('cors');
+
+const {
+    createCanvas,
+    loadImage
+} = require('canvas')
+const fs = require('fs')
+
 require('custom-env').env(true);
 
 
-//// posenet
-import * as posenet from '@tensorflow-models/posenet';
-async function estimatePoseOnImage(imageElement) {
-  // load the posenet model from a checkpoint
-  const net = await posenet.load();
 
-  const pose = await net.estimateSinglePose(imageElement, {
-    flipHorizontal: false
-  });
-  return pose;
+
+//// posenet
+// import posenet from '@tensorflow-models/posenet';
+const tf = require('@tensorflow/tfjs-node');
+const posenet = require('@tensorflow-models/posenet');
+async function estimatePoseOnImage(imageElement) {
+    const net = await posenet.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        inputResolution: {
+            width: 1280,
+            height: 720
+        },
+        multiplier: 0.75
+    });
+
+    const pose = await net.estimateMultiplePoses(imageElement, {
+        flipHorizontal: false
+    });
+    return pose;
 }
 
-const imageElement = document.getElementById('cat');
-const pose = estimatePoseOnImage(imageElement);
-console.log(pose);
+
+
+
+function randomNoRepeats(array) {
+  var copy = array.slice(0);
+  return function() {
+    if (copy.length < 1) { copy = array.slice(0); }
+    var index = Math.floor(Math.random() * copy.length);
+    var item = copy[index];
+    copy.splice(index, 1);
+    return item;
+  };
+}
 
 
 
 
-const net = await posenet.load({
-  architecture: 'MobileNetV1',
-  outputStride: 16,
-  inputResolution: { width: 640, height: 480 },
-  multiplier: 0.75
-});
+//// canvas
+const canvas = createCanvas(1280, 720);
+const ctx = canvas.getContext('2d');
+
+//// node-webcam (for testing)
+const NodeWebcam = require("node-webcam");
+var opts = {
+    width: 1280,
+    height: 720,
+    quality: 100,
+    saveShots: true,
+    output: "png",
+    device: false,
+    callbackReturn: "location",
+    verbose: true
+};
+var Webcam = NodeWebcam.create(opts);
+Webcam.capture( "test_picture", function( err, data ) {
+    console.log(data);
+    loadImage(data).then(async (image) => {
+        console.log("image", image);
+        ctx.drawImage(image, 0, 0);
+        var poses = await estimatePoseOnImage(canvas);
+
+
+        // console.log(pose)
+
+        var colors = ["#ffe900", "#ff3900", "cyan", "#18e074", "#359aff"];
+        var chooser = randomNoRepeats(colors);
+
+
+        for (var i = 0; i < poses.length; i ++) {
+            var pose = poses[i];
+            var score = pose.score;
+            console.log(i, score);
+            var points = pose.keypoints
+
+            if (score > 0.25) {
+                ctx.beginPath()
+
+                for (var j = 0; j < points.length; j++) {
+                    var point = points[j];
+                    var coords = point.position;
+                    // console.log(point);
+
+                    ctx.lineTo(coords.x, coords.y)
+                }
+
+                ctx.strokeStyle = chooser();
+                ctx.lineWidth = 2;
+                ctx.stroke()
+                ctx.closePath()
+            }
+        }
+
+        const out = fs.createWriteStream(__dirname + '/points.png')
+        const stream = canvas.createPNGStream()
+        stream.pipe(out)
+        out.on('finish', () =>  console.log('The PNG file was created.'))
+
+
+        // var keypoints = pose.keypoints;
+        // ctx.beginPath()
+        // for (var i = 0; i < keypoints.length; i++) {
+        //     var coords = keypoints[i].position;
+        //     console.log();
+        //     ctx.lineTo(coords.x, coords.y)
+        // };
+        // ctx.stroke()
+        //
+        // const out = fs.createWriteStream(__dirname + '/points.png')
+        // const stream = canvas.createPNGStream()
+        // stream.pipe(out)
+        // out.on('finish', () =>  console.log('The PNG file was created.'))
+
+
+    })
+} );
+
+
+
+//// realsense
+const rs2 = require('node-librealsense');
+const {GLFWWindow} = require('./glfw-window.js');
+const {glfw} = require('./glfw-window.js');
+
+function drawPointCloud(win, color, points) {
+  win.beginPaint();
+  if (points.vertices && points.textureCoordinates ) {
+    let count = points.size;
+    glfw.drawDepthAndColorAsPointCloud(
+        win.window,
+        new Uint8Array(points.vertices.buffer),
+        count,
+        new Uint8Array(points.textureCoordinates.buffer),
+        color.data,
+        color.width,
+        color.height,
+        'rgb8');
+  }
+  win.endPaint();
+}
+
+// // Open a GLFW window
+// const win = new GLFWWindow(1280, 720, 'Node.js PointCloud Example');
+// const pc = new rs2.PointCloud();
+// const pipeline = new rs2.Pipeline();
+//
+// pipeline.start();
+//
+// console.log('Drag to change perspective, scroll mouse wheel to zoom in/out.');
+//
+// while (! win.shouldWindowClose()) {
+//   const frameSet = pipeline.waitForFrames();
+//   const pointsFrame = pc.calculate(frameSet.depthFrame);
+//   pc.mapTo(frameSet.colorFrame);
+//   drawPointCloud(win, frameSet.colorFrame, pointsFrame);
+// }
+//
+// pc.destroy();
+// pipeline.stop();
+// pipeline.destroy();
+// win.destroy();
+//
+// rs2.cleanup();
+
+
+// A GLFW Window to display the captured image
+const win = new GLFWWindow(1280, 720, 'Node.js Capture Example');
+
+// Colorizer is used to map distance in depth image into different colors
+const colorizer = new rs2.Colorizer();
+
+// The main work pipeline of camera
+const pipeline = new rs2.Pipeline();
+
+// Start the camera
+pipeline.start();
+
+while (! win.shouldWindowClose()) {
+  const frameset = pipeline.waitForFrames();
+  // Build the color map
+  const depthMap = colorizer.colorize(frameset.depthFrame);
+  if (depthMap) {
+    // Paint the images onto the window
+    win.beginPaint();
+    const color = frameset.colorFrame;
+    glfw.draw2x2Streams(win.window, 2,
+        depthMap.data, 'rgb8', depthMap.width, depthMap.height,
+        color.data, 'rgb8', color.width, color.height);
+    win.endPaint();
+  }
+}
+
+pipeline.stop();
+pipeline.destroy();
+win.destroy();
+rs2.cleanup();
+
 
 
 //// Express Init
@@ -80,7 +261,9 @@ io.on('connection', function(client) {
     });
 
 
-    io.emit("global-init", {msg:"welcome"});
+    io.emit("global-init", {
+        msg: "welcome"
+    });
 
 
     //// socket badge scan
